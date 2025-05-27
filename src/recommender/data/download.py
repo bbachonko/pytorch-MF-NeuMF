bf15ec985b80694dfe75d31a8d3792cd1d7283fe
bartek.b <b.bartek37@gmail.com>
2025-05-27 12:36:45 +0000

    REF: added tags dataset and enymf trainer module

--- src/recommender/data/download.py
+++ src/recommender/data/download.py
@@ -9,10 +9,11 @@ from recommender.utils.logger import setup_logger
 logger = setup_logger(__name__)
 
 def load_data_from_kagglehub(dataset: str = "zygmunt/goodbooks-10k") -> str:
-    src_path = kagglehub.dataset_download(dataset)
-    logger.info(f"Kaggle Dataset: {dataset} was downloaded properly.")
-    
+    src_path = kagglehub.dataset_download(dataset)    
     dst_path = os.path.join(os.getcwd(), "goodbooks-10k")
+
+    logger.info(f"Kaggle Dataset: {dataset} was downloaded properly under path: {dst_path}")
+
     shutil.copytree(src_path, dst_path, dirs_exist_ok=True)
 
     return dst_path
--- src/recommender/scripts/train_neumf.py
+++ src/recommender/scripts/train_neumf.py
@@ -1,11 +1,15 @@
 import os
+from pathlib import Path
 import sys
+from dotenv import load_dotenv
 
 import pandas as pd
 
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from recommender.data.download import DATA_PATH
 from recommender.scripts.run_pipeline_neumf import run_pipeline_neumf
+
+load_dotenv()
 
 
 if __name__ == "__main__":
@@ -13,21 +17,25 @@ if __name__ == "__main__":
     ratings_df_sampled = ratings_df.sample(frac=0.25, random_state=42)
 
     books_df = pd.read_csv(DATA_PATH.joinpath("books.csv"))
-    # generalized_tags = 
 
+    # Referencing to prj root based on dynamic path since running on aml instance.
+    project_root = Path(os.environ["AML_INSTANCE_PATH_PREFIX"])  
+    tags_dataset_path = project_root.joinpath(
+        'pytorch-MF-NeuMF', 'src', 'recommender', 'datasets', 'generalized_tags.xlsx'
+    ).as_posix()
 
+    generalized_tags = pd.read_excel(tags_dataset_path)
 
-
-model_neumf, results, _ = run_pipeline_neumf(
-    ratings_df=ratings_df_sampled,
-    books_df=books_df,
-    # book_tags_df=book_tags_df,
-    # tags_df=tags_df,
-    generalized_tags=generalized_tags,
-    min_ratings= 5,
-    threshold= 4,
-    negatives_per_pos= 8,
-    eval_K= 10,
-    eval_n_neg= 999,
-    seed=42,
-)
+    model_neumf, results, _ = run_pipeline_neumf(
+        ratings_df=ratings_df_sampled,
+        books_df=books_df,
+        # book_tags_df=book_tags_df,
+        # tags_df=tags_df,
+        generalized_tags=generalized_tags,
+        min_ratings= 5,
+        threshold= 4,
+        negatives_per_pos= 8,
+        eval_K= 10,
+        eval_n_neg= 999,
+        seed=42,
+    )
